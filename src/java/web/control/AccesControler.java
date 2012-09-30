5e121117f1480d2368dcbd282ccd29966558320c
darthian <darthian@darthian>
2012-09-29 22:31:52 -0500

    Creacion aplicacion login con un solo usuario

---
+++ src/java/web/control/AccesControler.java
@@ -0,0 +1,39 @@
+package web.control;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class AccesControler {
+
+    public String login;
+    public String key;
+
+    public AccesControler() {
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String validar() {
+        String flag = "error";
+        if (this.login.equalsIgnoreCase("Ian") && this.key.equalsIgnoreCase("12345")) {
+            flag = "bienvenido";
+        }
+        return flag;
+    }
+}
